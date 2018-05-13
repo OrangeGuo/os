48a98d32da2f753b42c2903a20811ad13e7fba91
OrangeGuo <1772086465@qq.com>
2018-05-13 16:57:35 +0800

    add mysys.c

--- mysys.c
+++ mysys.c
@@ -1,9 +1,35 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 
-int main()
+int mysys(char* command)
 {
-    puts("999");
+    pid_t pid;
+    if(command==NULL)
+	return 1;
+    else if((pid=fork())<0)
+	return -1;
+    else if(pid==0){
+	execl("/bin/sh", "sh", "-c",command,NULL);
+	_exit(127);
+    }else{
+	pid_t wait_pid;
+	while((wait_pid=wait(NULL))<0){
+	    if(errno=EINTR)
+		return 0;
+	}
+    }
+    return 0;
+}
+
+int main(){
+    int mysys(char* command);
+    printf("--------------------\n");
+    mysys("echo HELLO WORLD");
+    printf("--------------------\n");
+    mysys("ls /");
+    printf("--------------------\n");
     return 0;
 }
